a025b7a0da9db254ac7428e8fa64438e85b789c3
Duc Huy Nguyen <stdcnguy>
2021-04-04 17:29:09 +0200

    added stringToBoardNotation + clean up

--- src/main/java/logic/BoardLogic.java
+++ src/main/java/logic/BoardLogic.java
@@ -5,39 +5,57 @@ import pojo.Cell;
 import pojo.Item;
 
 public class BoardLogic {
-    public static Board initSampleBoard(){
-        Board sampleBoard = initEmptyBoard(5,5);
-        addItem(sampleBoard, Item.BASKET, 3,2    );
-        addItem(sampleBoard, Item.BALL, 2,2);
-        addItem(sampleBoard, Item.WALL, 2,0);
+    public static Board initSampleBoard() {
+        Board sampleBoard = initEmptyBoard(5, 5);
+        addItem(sampleBoard, Item.BASKET, 4, 4);
+        addItem(sampleBoard, Item.BALL, 2, 2);
+        addItem(sampleBoard, Item.WALL, 2, 0);
         return sampleBoard;
     }
 
-    public static Board initEmptyBoard(int sizeI, int sizeJ){
+    public static Board initEmptyBoard(int sizeI, int sizeJ) {
         Board board = new Board(sizeI, sizeJ);
         initEmptyBoard(board);
         return board;
     }
 
-    private static void initEmptyBoard(Board board){
+    private static void initEmptyBoard(Board board) {
         board.setCells(new Cell[board.getSizeI()][board.getSizeJ()]);
-        for (int i = 0; i < board.getSizeI(); i++){
-            for (int j = 0; j < board.getSizeJ(); j++){
+        for (int i = 0; i < board.getSizeI(); i++) {
+            for (int j = 0; j < board.getSizeJ(); j++) {
                 board.getCells()[i][j] = new Cell();
             }
         }
     }
 
-    public static void printBoard(Board board){
-        for (int i = 0; i < board.getSizeI(); i++){
-            for (int j = 0; j < board.getSizeJ(); j++){
+    public static void printBoard(Board board) {
+        for (int i = 0; i < board.getSizeI(); i++) {
+            for (int j = 0; j < board.getSizeJ(); j++) {
                 System.out.print(board.getCells()[i][j]);
             }
             System.out.println();
         }
     }
 
-    public static void addItem(Board board, Item item, int i, int j){
+    public static void addItem(Board board, Item item, int i, int j) {
         board.getCells()[i][j] = new Cell(item);
+    }
+
+    public static Board stringNotationToBoard(String notation) {
+        String[] tokens = notation.split("/");
+
+        Board board = initEmptyBoard(tokens.length, tokens[0].length());
+        for (int i = 0; i < tokens.length; i++) {
+            for (int j = 0; j < tokens[i].length(); j++) {
+                if (tokens[i].charAt(j) == 'W') {
+                    addItem(board, Item.WALL, i, j);
+                } else if (tokens[i].charAt(j) == 'O') {
+                    addItem(board, Item.BALL, i, j);
+                } else if (tokens[i].charAt(j) == 'B') {
+                    addItem(board, Item.BASKET, i, j);
+                }
+            }
+        }
+        return board;
     }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,48 +1,12 @@
 import logic.BoardLogic;
-import logic.GameLogic;
-import logic.MovementLogic;
-import org.javatuples.Pair;
 import pojo.Board;
 import solver.Solver;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class main {
     public static void main(String[] args) {
-        Board board = BoardLogic.initSampleBoard();
-        Board tmp = BoardLogic.initSampleBoard();
-
-//        BoardLogic.printBoard(board);
+        Board board = BoardLogic.stringNotationToBoard("W.../W.W./..../O.B.");
 
         System.out.println(Solver.solve(board));
 
-//
-//        Set<Pair<Board, String>> test = Solver.getNextStates(board);
-//
-//        for (Pair<Board, String> data : test) {
-//            BoardLogic.printBoard(data.getValue0());
-//            System.out.println("--------------");
-//        }
-
-//        Pair<Board, String> first = Pair.with(board, "right");
-//        Pair<Board, String> second = Pair.with(board, "right");
-//
-//        Set<Pair<Board, String>> set = new HashSet<>();
-//
-//        set.add(first);
-//        set.add(second);
-//
-//        System.out.println(set.size());
-
-
-
-//        System.out.println(Solver.solve(board));
-
-//        Set<Board> boardSet = Solver.getNextStates(board);
-//        for (Board board1 : boardSet) {
-//            BoardLogic.printBoard(board1);
-//            System.out.println("----------");
-//        }
     }
 }
